5d3b3ce10ff26a1e75329bf5632b9d36fd0b62cb
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-13 13:12:10 +0530

    feat: setup server

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Api Working !" });
+});
+
+export default app;
