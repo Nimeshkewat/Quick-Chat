7cc504b2f29a19e4a854f7d8bb9000ebc55f6ab4
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-15 12:51:27 +0530

    Initialize socket server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,43 @@
 import "dotenv/config";
 import express from "express";
+import http from "node:http";
+import { Server } from "socket.io";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 
 const app = express();
+const server = http.createServer(app);
 
+//* Initialize socket server
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+//* Store online users
+export const userSocketMap: Record<string, string> = {};
+
+//* Socker connection handler
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User connected: ", userId);
+
+  if (userId) {
+    userSocketMap[userId.toString()] = socket.id;
+  }
+
+  //* Emit online users to all connected client
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", userId);
+    delete userSocketMap[userId?.toString()!];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+//* Middlewares
 app.use(express.json({ limit: "4mb" }));
 app.use(
   cors({
@@ -16,6 +47,7 @@ app.use(
 );
 app.use(cookieParser());
 
+//* Routes
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Api Working !" });
 });
