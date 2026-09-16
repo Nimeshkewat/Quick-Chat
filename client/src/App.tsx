7d5ddfa6037c35a1f78d0f2bdc020a856540e56c
Nimesh kewat <nimeshgamer37@gmail.com>
2026-09-16 12:31:35 +0530

    feat: add proteceted route in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  const { authUser, isCheckingAuth } = useAuth();
+  if (isCheckingAuth) return <h1>Loading...</h1>;
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain ">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
